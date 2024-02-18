82b94cce2551df2e8c42408f230806a9488ec2b8
challagondlaanilkumar <challagondlaanilkumar@gmail.com>
2024-02-18 12:58:15 +0530

    javascript updated

--- java.sh
+++ java.sh
@@ -1,56 +1,50 @@
 #!/bin/bash
-
+# apt install openjdk-11-jre-headless  # version 11.0.20.1+1-0ubuntu1~22.04, or
+# apt install default-jre              # version 2:1.11-72build2
+# apt install openjdk-17-jre-headless  # version 17.0.8.1+1~us1-0ubuntu1~22.04
+# apt install openjdk-18-jre-headless  # version 18.0.2+9-2~22.04
+# apt install openjdk-19-jre-headless  # version 19.0.2+7-0ubuntu3~22.04
+# apt install openjdk-8-jre-headless   # version 8u382-ga-1~22.04.1
 # Install Java 8
 install_java8() {
-    sudo apt-add-repository -y ppa:openjdk-r/ppa
     sudo apt-get update
-    sudo apt-get install -y openjdk-8-jdk
+    sudo openjdk-8-jre-headless -y
 }
 
 # Uninstall Java 8
 uninstall_java8() {
-    sudo apt-get remove -y openjdk-8-jdk
+    sudo apt-get remove -y openjdk-8-jre-headless
     sudo apt-get autoremove -y
 }
 
 # Install Java 11
 install_java11() {
-    sudo apt-get install -y openjdk-11-jdk
+    sudo apt-get install openjdk-11-jre-headless -y
 }
 
 # Uninstall Java 11
 uninstall_java11() {
-    sudo apt-get remove -y openjdk-11-jdk
+    sudo apt-get remove openjdk-11-jre-headless -y
     sudo apt-get autoremove -y
 }
 
 # Install Java 17
 install_java17() {
-    sudo add-apt-repository -y ppa:linuxuprising/java
     sudo apt-get update
-    sudo apt-get install -y jdk-17
+    sudo apt-get install openjdk-17-jre -y
 }
 
 # Uninstall Java 17
 uninstall_java17() {
-    sudo apt-get remove -y jdk-17
+    sudo apt-get remove openjdk-17-jre -y
     sudo apt-get autoremove -y
 }
 
-# Install Java 21
-install_java21() {
-    sudo apt-get install -y openjdk-21-jdk
-}
 
-# Uninstall Java 21
-uninstall_java21() {
-    sudo apt-get remove -y openjdk-21-jdk
-    sudo apt-get autoremove -y
-}
 
 # Prompt the user to enter the desired action and Java version
 read -p "Enter action (install|uninstall): " action
-read -p "Enter Java version (8|11|17|21): " java_version
+read -p "Enter Java version (8|11|17): " java_version
 
 # Call the appropriate function based on the input arguments
 if [ "$action" == "install" ]; then
@@ -60,8 +54,6 @@ if [ "$action" == "install" ]; then
         install_java11
     elif [ "$java_version" == "17" ]; then
         install_java17
-    elif [ "$java_version" == "21" ]; then
-        install_java21
     else
         echo "Invalid Java version: $java_version"
         exit 1
@@ -73,8 +65,6 @@ elif [ "$action" == "uninstall" ]; then
         uninstall_java11
     elif [ "$java_version" == "17" ]; then
         uninstall_java17
-    elif [ "$java_version" == "21" ]; then
-        uninstall_java21
     else
         echo "Invalid Java version: $java_version"
         exit 1
